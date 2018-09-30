fc62f0298ec504d49308767c8c39b54d0ce76c5c
phaoer <phaoer@sina.cn>
2018-09-30 16:16:18 +0800

    modify20180930

--- app/inx/inx.js
+++ app/inx/inx.js
@@ -1,12 +1,9 @@
-// var left=require("html-loader!./left.html");
-
 var right = require("html-loader!./right.html");
 
 module.exports = {
     render: function() {
         $(".pwh-inx-left img").attr("src", "http://ofad4ydvw.bkt.clouddn.com/io/mine.jpg");
         $(".pwh-inx-left h2").html("珍爱前端，远离IE");
-        // $(".pwh-body-content-left").html(left);
         $(".pwh-body-content-right").html(right);
         $("#pro,#fw,#wap,#essay").parent().removeClass("pwh-this");
         this.bind();
