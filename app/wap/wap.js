abe452333a8b00758bc5f4cf28a88009cd00de02
phaoer <phaoer@sina.cn>
2018-10-08 10:27:56 +0800

    modifyServer

--- app/wap/wap.js
+++ app/wap/wap.js
@@ -15,7 +15,7 @@ module.exports = {
 			return false;
         }
 
-        $.getJSON('http://47.95.10.105/index.php/githubio/index?callback=?',{type:'wap'},function(res){
+        $.getJSON('https://phaoer.imroc.io:8080/index.php/githubio/index?callback=?',{type:'wap'},function(res){
         	var wap = '<button class="back">返回目录</button><div class="pwh-list pwh-wap-list"><ul>';
         	res.list.map(function(ele,index){
                 wap += '<li><svg aria-hidden="true" class="octicon octicon-repo repo-icon" height="30" version="1.1" viewBox="0 0 12 16" width="20"><path fill-rule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg><a href="javascript:;" class="wap' + (index+1) + '">' + ele.title + '</a><p>' + ele.describle + '</p></li>';
